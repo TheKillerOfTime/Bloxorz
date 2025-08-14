dbe02c2e987540dbf18cc1e0db4b6fd25de362a8
TheKillerOfTime <ivanmatiasoggier@hotmail.com>
2025-08-14 17:19:54 -0300

    Va pa delante

--- Bloxorz/Assets/Scripts/PlayerMovement.cs
+++ Bloxorz/Assets/Scripts/PlayerMovement.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour{
-
-    //private float duracionRotacion = 0.5f;     // Tiempo en segundos que durará la rotación
-    //private bool horizontal=false, vertical=false;
-    //private Vector3 eje;
+    [Header("Logica de la rotacion")]
+    private bool isParado=true;
+    [SerializeField]private float duracionRotacionPlayer = 2f;      // Tiempo que tarda en rotar
+    private bool isHorizontal=false; //true: esta acostado  pero en vertical
+    //Si esta vertical cambia la X, si esta en horizontal la Z ---- EJ vertical: (0, 0.5, -1.5),,, EJ horizontal (0.5, 0.5,0)
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
@@ -13,47 +14,35 @@ public class PlayerMovement : MonoBehaviour{
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
-/*
-    public void IniciarRotacion(){
-        StartCoroutine(RotarRelativa(duracionRotacion));
+    void Update(){
+        controlarRotacionPlayer();
     }
 
-    IEnumerator RotarRelativa(float tiempo){
-        float gradosRotados = 0f;
-            float velocidadRotacion = grados / tiempo;  // grados por segundo
-            while (gradosRotados < grados){
-                float deltaGrados = velocidadRotacion * Time.deltaTime;
-                if (gradosRotados + deltaGrados > grados){
-                    deltaGrados = grados - gradosRotados;
-                    transform.RotateAround(centro, eje, deltaGrados);
-                    gradosRotados += deltaGrados;
-                    yield return null;
-                }
-            }
-    }
-
-    private void movimientos(){
-
+    private void controlarRotacionPlayer(){
         if(Input.GetKeyDown(KeyCode.W)){
+            if(isParado){
+                StartCoroutine(RotarEnUnTiempo(transform.position - new Vector3(0,1f,-0.5f), Vector3.right, 90));
+            }
+        }else if(Input.GetKeyDown(KeyCode.S)){
+
+        }else if(Input.GetKeyDown(KeyCode.D)){
+
+        }else if(Input.GetKeyDown(KeyCode.A)){
 
         }
+    }
 
-        if(Input.GetKeyDown(KeyCode.S)){
-
+    IEnumerator RotarEnUnTiempo(Vector3 punto, Vector3 eje, float anguloTotal){
+        float anguloRotado = 0f;
+        while (anguloRotado < Mathf.Abs(anguloTotal))
+        {
+            float anguloPaso = (Time.deltaTime / duracionRotacionPlayer) * Mathf.Abs(anguloTotal);
+            transform.RotateAround(punto, eje, anguloPaso * Mathf.Sign(anguloTotal));
+            anguloRotado += anguloPaso;
+            yield return null;
         }
+    }
 
-        if(Input.GetKeyDown(KeyCode.A)){
 
-        }
-
-        if(Input.GetKeyDown(KeyCode.D)){
-
-        }
-
-    }*/
 
 }
