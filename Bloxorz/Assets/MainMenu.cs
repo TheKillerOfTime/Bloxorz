154775fa2d6b4ae4183e496f1b379bc0a64044c0
lautarosoler <149752729+Lautsol@users.noreply.github.com>
2025-08-18 15:29:46 -0300

    Cambios en menu
    falta hacer funcionar el boton volver en opciones

--- Bloxorz/Assets/MainMenu.cs
+++ Bloxorz/Assets/MainMenu.cs
@@ -3,12 +3,25 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
+    public GameObject panelMenuPrincipal; // Inicio
+    public GameObject panelOpciones;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
-
+    public void AbrirOpciones()
+    {
+        Debug.Log(">>> AbrirOpciones ejecutado");
+        panelMenuPrincipal.SetActive(false);
+        panelOpciones.SetActive(true);
+    }
+    public void CerrarOpciones()
+    {
+        Debug.Log(">>> CerrarOpciones ejecutado");
+        panelOpciones.SetActive(false);
+        panelMenuPrincipal.SetActive(true);
+    }
     // Update is called once per frame
     void Update()
     {
