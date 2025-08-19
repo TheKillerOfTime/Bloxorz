36383f24f56ffe88e8f4181b1ae6c9f87855e1ed
TheKillerOfTime <ivanmatiasoggier@hotmail.com>
2025-08-19 15:31:57 -0300

    asf

--- Bloxorz/Assets/Scripts/GameManager.cs
+++ Bloxorz/Assets/Scripts/GameManager.cs
@@ -6,6 +6,9 @@ public class GameManager : MonoBehaviour{
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public static GameManager instance=null;
     private Cronometro cronometro;
+    private static bool aumentoNivel=false;
+    private static int nivel = 1;
+    [SerializeField] private TextMeshProUGUI nivelText;
     public TextMeshProUGUI timeText;
 
     void Start(){
@@ -17,12 +20,25 @@ public class GameManager : MonoBehaviour{
         }
         cronometro = new Cronometro(timeText, false);
         cronometro.activoDesactivo();
+        nivelText.text = "Nivel: " + nivel;
         
     }
 
     // Update is called once per frame
     void Update(){
         cronometro.correTimer();
+        repintoNivelText();
+    }
+
+    public static void incrementoNivel(){
+        nivel++;
+    }
+
+    private void repintoNivelText(){
+        if(aumentoNivel){
+            nivelText.text = "Nivel: " + nivel;
+            aumentoNivel=false;
+        }
     }
 
 }
